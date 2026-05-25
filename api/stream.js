a39521e29d09ecc6dd69f2bb57ec43e2b72fbc05
Vinicius <vinicius.feira@gmail.com>
2026-05-24 22:30:33 -0300

    Refactor streaming provider data retrieval

--- api/stream.js
+++ api/stream.js
@@ -1,5 +1,4 @@
 const axios = require('axios')
-
 const TMDB_KEY = process.env.TMDB_KEY
 
 module.exports = async (req, res) => {
@@ -16,7 +15,6 @@ module.exports = async (req, res) => {
     if (!results.length) return res.json({ streams: [] })
 
     const tmdbId = results[0].id
-    const title = results[0].title || results[0].name
     const endpoint = type === 'movie' ? 'movie' : 'tv'
 
     const { data } = await axios.get(`https://api.themoviedb.org/3/${endpoint}/${tmdbId}/watch/providers`, {
@@ -26,89 +24,32 @@ module.exports = async (req, res) => {
     const br = data.results?.BR
     if (!br) return res.json({ streams: [] })
 
-    const getProviderData = (providerId, title) => {
+    const getAppUris = (providerId, title) => {
       const encodedTitle = encodeURIComponent(title)
-      const providers = {
-        8: { // Netflix
-          url: `https://www.netflix.com/search?q=${encodedTitle}`,
-          uris: [
-            { platform: 'android', uri: `nflx://www.netflix.com/search?q=${encodedTitle}` },
-            { platform: 'ios', uri: `nflx://www.netflix.com/search?q=${encodedTitle}` }
-          ]
-        },
-        1796: { // Netflix Ads
-          url: `https://www.netflix.com/search?q=${encodedTitle}`,
-          uris: [
-            { platform: 'android', uri: `nflx://www.netflix.com/search?q=${encodedTitle}` },
-            { platform: 'ios', uri: `nflx://www.netflix.com/search?q=${encodedTitle}` }
-          ]
-        },
-        337: { // Disney+
-          url: `https://www.disneyplus.com/search?q=${encodedTitle}`,
-          uris: [
-            { platform: 'android', uri: `disneyplus://search?q=${encodedTitle}` },
-            { platform: 'ios', uri: `disneyplus://search?q=${encodedTitle}` }
-          ]
-        },
-        119: { // Prime Video
-          url: `https://www.primevideo.com/search?phrase=${encodedTitle}`,
-          uris: [
-            { platform: 'android', uri: `primevideo://search?q=${encodedTitle}` },
-            { platform: 'ios', uri: `primevideo://search?q=${encodedTitle}` }
-          ]
-        },
-        1899: { // Max
-          url: `https://play.max.com/search?q=${encodedTitle}`,
-          uris: [
-            { platform: 'android', uri: `wbdstreaming://search?q=${encodedTitle}` },
-            { platform: 'ios', uri: `wbdstreaming://search?q=${encodedTitle}` }
-          ]
-        },
-        1825: { // Max Amazon Channel
-          url: `https://play.max.com/search?q=${encodedTitle}`,
-          uris: [
-            { platform: 'android', uri: `wbdstreaming://search?q=${encodedTitle}` },
-            { platform: 'ios', uri: `wbdstreaming://search?q=${encodedTitle}` }
-          ]
-        },
-        307: { // Globoplay
-          url: `https://globoplay.globo.com/busca/?q=${encodedTitle}`,
-          uris: [
-            { platform: 'android', uri: `globoplay://busca/?q=${encodedTitle}` },
-            { platform: 'ios', uri: `globoplay://busca/?q=${encodedTitle}` }
-          ]
-        },
-        350: { // Apple TV+
-          url: `https://tv.apple.com/br/search?term=${encodedTitle}`,
-          uris: [
-            { platform: 'android', uri: `atve://search?term=${encodedTitle}` },
-            { platform: 'ios', uri: `atve://search?term=${encodedTitle}` }
-          ]
-        },
-        531: { // Paramount+
-          url: `https://www.paramountplus.com/br/search/?q=${encodedTitle}`,
-          uris: [
-            { platform: 'android', uri: `pplus://search?q=${encodedTitle}` },
-            { platform: 'ios', uri: `pplus://search?q=${encodedTitle}` }
-          ]
-        }
+      const uris = {
+        8:    [{ platform: 'android', uri: `nflx://www.netflix.com/search?q=${encodedTitle}` }, { platform: 'ios', uri: `nflx://www.netflix.com/search?q=${encodedTitle}` }],
+        1796: [{ platform: 'android', uri: `nflx://www.netflix.com/search?q=${encodedTitle}` }, { platform: 'ios', uri: `nflx://www.netflix.com/search?q=${encodedTitle}` }],
+        337:  [{ platform: 'android', uri: `disneyplus://search?q=${encodedTitle}` }, { platform: 'ios', uri: `disneyplus://search?q=${encodedTitle}` }],
+        119:  [{ platform: 'android', uri: `primevideo://search?q=${encodedTitle}` }, { platform: 'ios', uri: `primevideo://search?q=${encodedTitle}` }],
+        1899: [{ platform: 'android', uri: `wbdstreaming://search?q=${encodedTitle}` }, { platform: 'ios', uri: `wbdstreaming://search?q=${encodedTitle}` }],
+        1825: [{ platform: 'android', uri: `wbdstreaming://search?q=${encodedTitle}` }, { platform: 'ios', uri: `wbdstreaming://search?q=${encodedTitle}` }],
+        307:  [{ platform: 'android', uri: `globoplay://busca/?q=${encodedTitle}` }, { platform: 'ios', uri: `globoplay://busca/?q=${encodedTitle}` }],
+        350:  [{ platform: 'android', uri: `atve://search?term=${encodedTitle}` }, { platform: 'ios', uri: `atve://search?term=${encodedTitle}` }],
+        531:  [{ platform: 'android', uri: `pplus://search?q=${encodedTitle}` }, { platform: 'ios', uri: `pplus://search?q=${encodedTitle}` }],
       }
-      return providers[providerId] || { url: br.link, uris: [] }
+      return uris[providerId] || []
     }
 
     const seen = new Set()
     const streams = (br.flatrate || [])
       .filter(p => !seen.has(p.provider_id) && seen.add(p.provider_id))
-      .map(p => {
-        const providerData = getProviderData(p.provider_id, title)
-        return {
-          name: 'Onde Assistir BR',
-          type: 'other',
-          title: `Assistir no ${p.provider_name}`,
-          externalUrl: providerData.url,
-          externalUris: providerData.uris
-        }
-      })
+      .map(p => ({
+        name: 'Onde Assistir BR',
+        type: 'other',
+        title: `Assistir no ${p.provider_name}`,
+        externalUrl: br.link,
+        externalUris: getAppUris(p.provider_id, results[0].title || results[0].name)
+      }))
 
     res.json({ streams })
   } catch (e) {
